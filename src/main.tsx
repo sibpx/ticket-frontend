f5ddefd95e62b0e29300f111c29d879b38e16e87
sibpx <mikulin1999@gmail.com>
2024-08-03 17:05:53 +0500

    #3 Реализована шапка сайта

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "app";
+import { App, theme } from "app";
+import "shared/styles/index.css";
+import { ColorModeScript } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <App />
   </React.StrictMode>
 );
--- src/widgets/header/header.tsx
+++ src/widgets/header/header.tsx
@@ -1,3 +1,52 @@
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  IconButton,
+  Spacer,
+  useColorMode,
+} from "@chakra-ui/react";
+import { NavLink } from "react-router-dom";
+import { routes } from "shared";
+
 export const Header = () => {
-  return <>header</>;
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <Box bgColor="blackAlpha.400" as="header">
+      <Flex py="1rem" w="80%" mx="auto">
+        <Heading as={NavLink} to={routes.base}>
+          Ticket
+        </Heading>
+        <Spacer />
+
+        <Flex gap="0.5rem">
+          <Button colorScheme="blue" size="md">
+            Новый тикет
+          </Button>
+          <NavLink to={routes.current}>
+            {({ isActive }) => (
+              <Button isDisabled={isActive}>Текущие тикеты</Button>
+            )}
+          </NavLink>
+          <NavLink to={routes.history}>
+            {({ isActive }) => <Button isDisabled={isActive}>История</Button>}
+          </NavLink>
+          <NavLink to={routes.account}>
+            {({ isActive }) => (
+              <Button isDisabled={isActive} colorScheme="teal">
+                Аккаунт
+              </Button>
+            )}
+          </NavLink>
+          <IconButton
+            onClick={toggleColorMode}
+            icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+            aria-label="Сменить тему"
+          />
+        </Flex>
+      </Flex>
+    </Box>
+  );
 };
